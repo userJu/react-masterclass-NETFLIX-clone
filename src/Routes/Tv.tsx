749ec87eb664c2fcd2d654876d4496737d208b53
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-01-27 11:13:24 +0900

    react-router-dom and make navbar with css

---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,15 @@
+const Tv = () => {
+  return (
+    <div>
+      <h1>Tv</h1>
+      <h1>Tv</h1>
+      <h1>Tv</h1>
+      <h1>Tv</h1>
+      <h1>Tv</h1>
+      <h1>Tv</h1>
+      <h1>Tv</h1>
+    </div>
+  );
+};
+
+export default Tv;
---
+++ src/theme.ts
@@ -0,0 +1,44 @@
+export const theme = {
+  red: "#E51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
+};
+// import { DefaultTheme } from "styled-components";
+
+// export const theme: DefaultTheme = {
+//   red: "#E51013",
+//   black: {
+//     veryDark: "#141414",
+//     darker: "#181818",
+//     lighter: "#2F2F2F",
+//   },
+//   white: {
+//     lighter: "#fff",
+//     darker: "#e5e5e5",
+//   },
+// };
+
+// styled.data.ts,
+// import "styled-components";
+
+// declare module "styled-components" {
+//   export interface DefaultTheme {
+//     red: string;
+//     black: {
+//       veryDark: string;
+//       darker: string;
+//       lighter: string;
+//     };
+//     white: {
+//       darker: string;
+//       lighter: string;
+//     };
+//   }
+// }
